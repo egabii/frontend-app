863502d053eeb543072fc8025e500cfd2078aad5
gabriel.muller <gabriel.muller@globant.com>
2019-01-11 18:00:23 -0300

    Componize Home and About page.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,18 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 
+import AlertButton  from '../../common/AlertButton/AlertButton';
+
 const Home = () => {
+    const triggerAlert = () => {
+        alert('Hello World');
+    };
+
     return (
         <div className="homepage">
             <h1>Home</h1>
             <p>Hi this is the home page</p>
+            <AlertButton clickHandler={triggerAlert} label={'This is a button'} />
         </div>
     );
 }
